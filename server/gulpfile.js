be62a08486a654c0f4377a4a3e89e4f8db7a4a90
Krystian Gradowski <krystian.grado93@gmail.com>
2016-06-01 14:45:39 +0200

    server

--- server/gulpfile.js
+++ server/gulpfile.js
@@ -1,4 +1,7 @@
 var gulp = require('gulp');
+var concat = require('gulp-concat');
+var jshint = require('gulp-jshint');
+var watch = require('gulp-watch');
 
 
 gulp.task('copy_task', function() {
@@ -18,6 +21,58 @@ gulp.task('copy_task', function() {
     .pipe(gulp.dest('../build/lib'));
     gulp.src('partials/*.html')
     .pipe(gulp.dest('../build/partials'));
+});
+
+gulp.task('build:dev', function() {
+    gulp.src('*.html')
+    .pipe(gulp.dest('../build/'));
+    gulp.src('*.js')
+    .pipe(gulp.dest('../build/'));
+    gulp.src('*.json')
+    .pipe(gulp.dest('../build/'));
+    gulp.src('css/*.css')
+    .pipe(gulp.dest('../build/css'));
+    gulp.src('js/*.js')
+    .pipe(gulp.dest('../build/js'));
+    gulp.src('lib/*.js')
+    .pipe(gulp.dest('../build/lib'));
+});
+
+gulp.task('build:prod', function() {
+    gulp.src('*.html')
+    .pipe(gulp.dest('../build/'));
+    gulp.src('*.js')
+    .pipe(gulp.dest('../build/'));
+    gulp.src('*.json')
+    .pipe(gulp.dest('../build/'));
+    gulp.src('css/*.css')
+    .pipe(gulp.dest('../build/css'));
+    gulp.src('js/*.js')
+    .pipe(concat('app.js'))
+    .pipe(gulp.dest('../build/js'));
+    gulp.src('lib/*.js')
+    .pipe(gulp.dest('../build/lib'));
+});
+
+gulp.task('jshint', function() {
+  return gulp.src('./js/*.js')
+    .pipe(jshint())
+    .pipe(jshint.reporter('default'));
+});
+
+gulp.task('stream', function () {
+    return gulp.src('*.html')
+        .pipe(watch('*.html'))
+        .pipe(gulp.dest('../build')),
+         gulp.src('partials/*.html')
+        .pipe(watch('partials/*.html'))
+        .pipe(gulp.dest('../build/partials')),
+        gulp.src('css/*.css')
+        .pipe(watch('css/*.css'))
+        .pipe(gulp.dest('../build/css')),
+        gulp.src('js/*.js')
+        .pipe(watch('js/*.js'))
+        .pipe(gulp.dest('../build/js'));
 });
 
 
